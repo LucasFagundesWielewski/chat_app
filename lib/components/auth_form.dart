ad5d7cdbd083eec99afbc015df2ada44cdf94c66
Lucas <lucas.fwielewski@gmail.com>
2024-12-18 23:41:59 -0300

    Refactor AuthForm to use StatefulWidget and implement form key for validation

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,16 @@
+import 'package:chat_app/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +19,7 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(

935f464b08d2b7925554438e6138948bb1b3c9d0
Lucas <lucas.fwielewski@gmail.com>
2024-12-23 23:55:04 -0300

    Implement message streaming in Messages component with loading and empty state handling

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/core/models/chat_messages.dart';
+import 'package:chat_app/core/models/services/auth/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,8 +7,23 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Mensagens'),
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messagesStram(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (!snapshot.hasData) {
+          return const Center(
+            child: Text('Sem Dados. Vamos conversar?'),
+          );
+        } else {
+          final msgs = snapshot.data!;
+          return ListView.builder(
+            itemCount: msgs.length,
+            itemBuilder: (ctx, i) => Text(msgs[i].text),
+          );
+        }
+      },
     );
   }
 }

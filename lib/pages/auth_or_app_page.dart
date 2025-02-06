2957783969d79e43d5d3653f51ca496adfc40865
Lucas <lucas.fwielewski@gmail.com>
2025-02-05 23:17:39 -0300

    Add ChatNotificationService for handling Firebase messaging and update AuthOrAppPage to initialize notifications

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,16 +1,23 @@
 import 'package:chat_app/core/models/chat_user.dart';
 import 'package:chat_app/core/models/services/auth/auth_service.dart';
+import 'package:chat_app/core/models/services/notification/chat_notification_service.dart';
 import 'package:chat_app/pages/auth_page.dart';
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/loading_pages.dart';
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+    await Provider.of<ChatNotificationService>(
+      context,
+      listen: false,
+    ).init();
   }
 
   @override
